694a9fb60931389fcbccc51100e010181cabedc1
TypeTew <tewsk132@gmail.com>
2024-04-24 20:29:25 +0700

    update

--- Controllers/AuthenticateController.cs
+++ Controllers/AuthenticateController.cs
@@ -1,8 +1,10 @@
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using StoreAPI.Models;
 
